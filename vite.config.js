2897e1713182469dd5634aacbfb9a0f1e5fc8797
Caroline de Jesus <161940709+carolinejesus@users.noreply.github.com>
2025-12-10 09:25:14 -0300

    Remove build settings from vite.config.js
    Removed build configuration settings from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  build: {
-    outDir: 'dist',   
-    minify: 'esbuild', 
-    sourcemap: true,  
-  },
   optimizeDeps: {
     include: ['react', 'react-dom']
   }
